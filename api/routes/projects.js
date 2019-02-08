fbfae8e9c85a61cdd4b11b57e17b7dbfde5dd0bc
ThanhNT <thanh.nt@niteco.se>
2019-02-08 11:01:10 +0700

    Add react app to work with express api

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('projects router');
+  res.render('index', { title: 'Task Management' });
+});
+router.get('/:task', function(req, res, next) {
+  res.render('index', { title: 'Task Management' });
 });
 
 module.exports = router;
