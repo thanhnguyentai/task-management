5d4970245d29ca6186a717fb49821adde0b3ce9f
ThanhNT <thanh.nt@niteco.se>
2019-02-17 15:04:01 +0700

    Complete user service

--- api/services/users.js
+++ api/services/users.js
@@ -38,14 +38,65 @@ class UserService {
     }
 
     updateInfo(id, name) {
-        
+        return new Promise((resolve, reject) => {
+            if(!id || !name) {
+                reject({error: 'Id and name are required'});
+            } else {
+                UserModel.findByIdAndUpdate(id, {
+                    name: name
+                }).then(doc => {
+                    resolve({
+                        message: 'Update info successfully'
+                    });
+                }).catch(err => {
+                    reject({
+                        error: 'Update info failed'
+                    });
+                })
+            }
+        });
     }
 
     updatePassword(id, password) {
-
+        return new Promise((resolve, reject) => {
+            if(!id || !password) {
+                reject({error: 'Id and password are required'});
+            } else {
+                UserModel.findByIdAndUpdate(id, {
+                    password: md5(password)
+                }).then(doc => {
+                    resolve({
+                        message: 'Update password successfully'
+                    });
+                }).catch(err => {
+                    reject({
+                        error: 'Update password failed'
+                    });
+                })
+            }
+        });
     }
 
-    login() {}
+    login(email, password) {
+        return new Promise((resolve, reject) => {
+            if(!email || !password) {
+                reject({error: 'email and password are required'});
+            } else if(!UserHelper.emailIsValid(email)) {
+                reject({error: 'Email is invalid'});
+            }else {
+                UserModel.findOne({
+                    email: email,
+                    password: md5(password)
+                }).then(doc => {
+                    resolve(doc);
+                }).catch(err => {
+                    reject({
+                        error: 'Cannot login, email or password is wrong.'
+                    });
+                })
+            }
+        });
+    }
 }
 
 export default new UserService();
