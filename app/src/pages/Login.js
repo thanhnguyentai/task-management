8de9a051d968c05702f27caa3699265591cdbdb5
ThanhNT <thanh.nt@niteco.se>
2019-02-21 23:26:27 +0700

    Add error message when login/register

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
 import validator from 'email-validator'
-import { Button, Form } from 'semantic-ui-react';
+import { Button, Form, Message } from 'semantic-ui-react';
 import './Login.scss';
 import UserService from '../service/userService';
 
 function Login(props) {
     const [isLogin, setLogin] = useState(true);
+    const [error, setError] = useState('');
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [password2, setPassword2] = useState('');
 
     const register = function() {
+        setError('');
         setLogin(false);
     }
 
     const login = function() {
+        setError('');
         setLogin(true);
     }
 
     const onChangeName = function(event) {
-        setName(event.target.value);
+        setName(event.target.value.trim());
     }
 
     const onChangeEmail = function(event) {
-        setEmail(event.target.value);
+        setEmail(event.target.value.trim());
     }
 
     const onChangePassword = function(event){
@@ -37,6 +40,7 @@ function Login(props) {
 
     const doLogin = function() {
         if(email && validator.validate(email) && password) {
+            setError('');
             UserService.login(email, password).then(data => {
                 if(data.id) {
                     props.onLogin(data);
@@ -44,16 +48,31 @@ function Login(props) {
             }).catch(err => {
                 console.log(err.response);
             });
+        } else if(!email || !validator.validate(email)) {
+            setError('.please check email');
+        } else {
+            setError('.please check password');
         }
     }
 
-    const doRegister = function() {}
+    const doRegister = function() {
+        if(name && email && validator.validate(email) && password && password2 && password === password2) {
+            setError('');
+        } else if(!name) {
+            setError('.please check name');
+        } else if(!email || !validator.validate(email)) {
+            setError('.please check email');
+        } else if(!password || !password2 || password !== password2) {
+            setError('.please check password');
+        }
+    }
 
     return (
         <div className='login-wrapper'>
             <Form>
                 {isLogin && <h1>Login</h1>}
                 {!isLogin && <h1>Create an account</h1>}
+                {error && <Message color='red'>{error}</Message>}
                 {
                     !isLogin && 
                     <Form.Field>
